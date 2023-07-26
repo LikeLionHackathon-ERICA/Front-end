8b8480f3ea0e86343ecf793da33fb276a9cd3f94
bishoe01 <bishoe01@gmail.com>
2023-07-26 13:27:57 +0900

    Add : tailwindcss,react-router-dom 설치 , Login Page구현

--- likedigital/src/main.jsx
+++ likedigital/src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
 import './index.css'
+import Home from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // errorElement: <Error/>,
+    children: [{ index: true, path: '/', element: <Home /> },
+    { path: 'login', element: <Login /> }
+  ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
---
+++ likedigital/src/pages/Login.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleIdChange = (e) => setId(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`ID: ${id}, Password: ${password}`);
+  };
+
+  return (
+    <div className="min-h-screen flex justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Login
+          </h2>
+        </div>
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+          <input type="hidden" name="remember" value="true" />
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="user-id" className="sr-only">
+                ID
+              </label>
+              <input
+                id="user-id"
+                name="id"
+                type="text"
+                autoComplete="username"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="ID"
+                value={id}
+                onChange={handleIdChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="text"
+                autoComplete="current-password"
+                required
+                className=" appearance-none rounded-none relative border block w-full px-3 py-2 text-black  border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              LOG IN
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
