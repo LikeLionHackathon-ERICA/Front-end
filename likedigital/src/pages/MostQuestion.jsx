f9b7ca53ab302ec1bf2c88c7b1addc2eacf05b85
Yoonjit <jisu9083@naver.com>
2023-08-05 03:24:27 +0900

    MQ - 이미지 추가

--- likedigital/src/pages/MostQuestion.jsx
+++ likedigital/src/pages/MostQuestion.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
+import underline from "../assets/underline.png";
+import toggle from "../assets/toggle.png";
+
 function MostQusetion() {
   return (
     <div className="mq_container">
       <h3 className="problem1">많이 검색한 질문</h3>
+
+      <img src={underline} alt="guideslide" />
+      <img src={toggle} alt="toggle" />
     </div>
   );
 }
