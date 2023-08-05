2fed786a60ce99bfcc4ce457422c7ec361a0414f
Yoonjit <jisu9083@naver.com>
2023-08-06 03:01:31 +0900

    모든 컴포넌트 생성, 퍼블리싱 완료

--- likedigital/src/pages/MostQuestion.jsx
+++ likedigital/src/pages/MostQuestion.jsx
@@ -5,14 +5,67 @@ import underline from "../assets/underline.png";
 import togglewhite from "../assets/toggle-white.png";
 import togglegray from "../assets/toggle-gray.png";
 
+function Rank({ user }) {
+  return (
+    <div className="question-box">
+      <h3>{user.rank}</h3>
+      <h3>{user.text}</h3>
+      <img className="togglegray" src={togglegray} alt="togglegray" />
+    </div>
+  );
+}
+
 function MostQusetion() {
+  const users = [
+    {
+      id: 1,
+      rank: "01",
+      text: "질문 1~~~",
+    },
+    {
+      id: 2,
+      rank: "02",
+      text: "질문 2~~~",
+    },
+    {
+      id: 3,
+      rank: "03",
+      text: "질문 3~~~",
+    },
+  ];
+
   return (
     <div className="mq_container">
       <h3 className="problem1">많이 검색한 질문</h3>
 
-      <img src={underline} alt="guideslide" />
-      <img src={togglewhite} alt="toggle-white" />
-      <img src={togglegray} alt="toggle-gray" />
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div style={{ display: "flex" }}>
+          <div>
+            <button className="count-button1">최다 조회수</button>
+            <img className="underline1" src={underline} alt="underline" />
+          </div>
+          <div>
+            <button className="count-button2">공감수</button>
+            <img className="underline2" src={underline} alt="underline" />
+          </div>
+        </div>
+        <div style={{ marginTop: "18px" }}>
+          <button className="sort-button">
+            인기순{" "}
+            <img className="togglewhite" src={togglewhite} alt="togglewhite" />
+          </button>
+        </div>
+      </div>
+
+      <div>
+        {users.map((user) => (
+          <Rank user={user} />
+        ))}
+      </div>
+
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button className="more-button">더보기</button>
+      </div>
     </div>
   );
 }
--- likedigital/src/pages/Problem.jsx
+++ likedigital/src/pages/Problem.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import bear1 from "../assets/bear1.png";
+import question from "../assets/question.png";
 
 function Problem() {
   return (
@@ -12,13 +13,21 @@ function Problem() {
       </div>
       <div className="problem-section-2">
         <div className="problem-box1">
+          <h3 style={{ color: "#a0d0f1" }}>01</h3>
           <h3>문제 등록은 어떻게?</h3>
         </div>
         <div className="problem-box2">
+          <h3 style={{ color: "#d1efdf" }}>02</h3>
           <h3>등록한 문제 어디서?</h3>
         </div>
       </div>
       <div className="problem-box3">
+        <img
+          className="question"
+          src={question}
+          alt="question"
+          style={{ width: "15px" }}
+        />
         <h3>다른 질문이 있어요!</h3>
         <img className="bear1" src={bear1} alt="bear1" />
       </div>
