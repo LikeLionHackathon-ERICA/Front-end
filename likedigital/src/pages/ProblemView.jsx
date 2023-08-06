6d5a86962e159e2e8fd630ad300db73fc4098858
bishoe01 <bishoe01@gmail.com>
2023-08-06 16:04:38 +0900

    [ADD] ProblemCard

--- likedigital/src/pages/ProblemView.jsx
+++ likedigital/src/pages/ProblemView.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import { useState } from "react";
 import ProblemCard from "../components/problem/ProblemCard";
+import { useEffect } from "react";
+import axios from "axios";
 
 export default function ProblemView() {
+  const [problems, setProblems] = useState([]);
+  useEffect(() => {
+    axios
+      .get("/data/problems.json")
+      .then((res) => {
+        setProblems(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
-    <div className="flex flex-col justify-center p-2">
-      <ProblemCard />
+    <div className="flex flex-col justify-center p-2 gap-2">
+      {problems.map((problem, index) => (
+        <ProblemCard
+          key={index}
+          title={problem.title}
+          phoneNumber={problem.phoneNumber}
+          distance={problem.distance}
+          status={problem.status}
+          category={problem.category}
+        />
+      ))}
     </div>
   );
 }
